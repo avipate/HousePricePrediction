01152ee690c5297376f42bd67d8b616696da3669
Avi Patel <avip1607@outlook.com>
2023-10-31 13:18:43 +0530

    Add: Feature engineering part 2 completed

--- model.py
+++ model.py
@@ -86,8 +86,8 @@ class HousePricePrediction:
             print(var, mean_val)
 
             # add binary missing indicator (in train and test)
-            self.x_train[var + '_na'] = np.where(self.x_train[var].isnull(), 1, 0)
-            self.x_test[var + '_na'] = np.where(self.x_test[var].isnull(), 1, 0)
+            self.x_train[var + "_na"] = np.where(self.x_train[var].isnull(), 1, 0)
+            self.x_test[var + "_na"] = np.where(self.x_test[var].isnull(), 1, 0)
 
             # replace missing values by the mean (in train and test)
             self.x_train[var].fillna(mean_val, inplace=True)
@@ -97,23 +97,25 @@ class HousePricePrediction:
         print(self.x_train[var_with_na].isnull().sum())
         print(self.x_test[var_with_na].isnull().sum())
 
-        print(self.x_train[['LotFrontage_na', 'MasVnrArea_na', 'GarageYrBlt_na']].head())
+        print(
+            self.x_train[["LotFrontage_na", "MasVnrArea_na", "GarageYrBlt_na"]].head()
+        )
 
         # Part 2
         # Capture elapsed time
         def elapsed_year(df, var):
             # capture difference between the year variable
             # and the year in which the house was sold
-            df[var] = df['YrSold'] - df[var]
+            df[var] = df["YrSold"] - df[var]
             return df
 
-        for var in ['YearBuilt', 'YearRemodAdd', 'GarageYrBlt']:
+        for var in ["YearBuilt", "YearRemodAdd", "GarageYrBlt"]:
             self.x_train = elapsed_year(self.x_train, var)
             self.x_test = elapsed_year(self.x_test, var)
 
         # now we drop YrSold
-        self.x_train.drop(['YrSold'], axis=1, inplace=True)
-        self.x_test.drop(['YrSold'], axis=1, inplace=True)
+        self.x_train.drop(["YrSold"], axis=1, inplace=True)
+        self.x_test.drop(["YrSold"], axis=1, inplace=True)
 
         # Numerical variable transformation
         # Logarithmic transformation
@@ -122,17 +124,174 @@ class HousePricePrediction:
             self.x_test[var] = np.log(self.x_test[var])
 
         # Check that test set does not contain null values in the engineered variables
-        null = [var for var in ["LotFrontage", "1stFlrSF", "GrLivArea"] if self.x_test[var].isnull().sum()]
+        null = [
+            var
+            for var in ["LotFrontage", "1stFlrSF", "GrLivArea"]
+            if self.x_test[var].isnull().sum()
+        ]
         print(null)
 
-        null = [var for var in ["LotFrontage", "1stFlrSF", "GrLivArea"] if self.x_train[var].isnull().sum()]
+        null = [
+            var
+            for var in ["LotFrontage", "1stFlrSF", "GrLivArea"]
+            if self.x_train[var].isnull().sum()
+        ]
+        print(null)
+
+        # Binarize skewed variables
+        skewed = [
+            "BsmtFinSF2",
+            "LowQualFinSF",
+            "EnclosedPorch",
+            "3SsnPorch",
+            "ScreenPorch",
+            "MiscVal",
+        ]
+
+        for var in skewed:
+            # map the variable values into 0 to 1
+            self.x_train[var] = np.where(self.x_train[var] == 0, 0, 1)
+            self.x_test[var] = np.where(self.x_test[var] == 0, 0, 1)
+
+        # Categorical variables
+        # Apply mapping
+        # re-map strings to numbers, which determine quality
+        qual_mappings = {
+            "Po": 1,
+            "Fa": 2,
+            "TA": 3,
+            "Gd": 4,
+            "Ex": 5,
+            "Missing": 0,
+            "NA": 0,
+        }
+
+        qual_vars = [
+            "ExterQual",
+            "ExterCond",
+            "BsmtQual",
+            "BsmtCond",
+            "HeatingQC",
+            "KitchenQual",
+            "FireplaceQu",
+            "GarageQual",
+            "GarageCond",
+        ]
+
+        for var in qual_vars:
+            self.x_train[var] = self.x_train[var].map(qual_mappings)
+            self.x_test[var] = self.x_test[var].map(qual_mappings)
+
+        exposure_mappings = {"No": 1, "Mn": 2, "Av": 3, "Gd": 4}
+        var = "BsmtExposure"
+
+        self.x_train[var] = self.x_train[var].map(exposure_mappings)
+        self.x_test[var] = self.x_test[var].map(exposure_mappings)
+
+        finish_mappings = {
+            "Missing": 0,
+            "NA": 0,
+            "Unf": 1,
+            "LwQ": 2,
+            "Rec": 3,
+            "BLQ": 4,
+            "ALQ": 5,
+            "GLQ": 6,
+        }
+        finish_var = ["BsmtFinType1", "BsmtFinType2"]
+
+        for var in finish_var:
+            self.x_train[var] = self.x_train[var].map(finish_mappings)
+            self.x_test[var] = self.x_test[var].map(finish_mappings)
+
+        garage_mappings = {"Missing": 0, "NA": 0, "Unf": 1, "RFn": 2, "Fin": 3}
+
+        var = "GarageFinish"
+
+        self.x_train[var] = self.x_train[var].map(garage_mappings)
+        self.x_test[var] = self.x_test[var].map(garage_mappings)
+
+        fence_mappings = {
+            "Missing": 0,
+            "NA": 0,
+            "MnWw": 1,
+            "GdWo": 2,
+            "MnPrv": 3,
+            "GdPrv": 4,
+        }
+        var = "Fence"
+
+        self.x_train[var] = self.x_train[var].map(fence_mappings)
+        self.x_test[var] = self.x_test[var].map(fence_mappings)
+
+        null = [
+            var for var in self.x_train.columns if self.x_train[var].isnull().sum() > 0
+        ]
+        print(null)
+
+        # Remove Rare Labels
+        # Capture all quality variables
+        qual_vars = qual_vars + finish_var + ["BsmtExposure", "GarageFinish", "Fence"]
+
+        # capture the remaining categorical variables
+        # (those that we did not re-map)
+        cat_others = [var for var in cat_vars if var not in qual_vars]
+
+        print(len(cat_others))
+
+        # Function that fins the labels that are shared by more than a certain % of the house in the dataset
+        def find_frequent_labels(df, var, rare_perc):
+            self.df = self.df.copy()
+
+            tmp = self.df.groupby(var)[var].count() / len(self.df)
+
+            return tmp[tmp > rare_perc].index
+
+        for var in cat_others:
+            # find the frequent categories
+            frequent_ls = find_frequent_labels(self.x_train, var, 0.01)
+            print(var, frequent_ls)
+
+            # replace rare categories by the string "Rare"
+            self.x_train[var] = np.where(
+                self.x_train[var].isin(frequent_ls), self.x_train[var], "Rare"
+            )
+            self.x_test[var] = np.where(
+                self.x_test[var].isin(frequent_ls), self.x_test[var], "Rare"
+            )
+
+        # Encoding of categorical variables
+        def replace_categories(train, test, y_train, var, target):
+            tmp = pd.concat([self.x_train, y_train], axis=1)
+
+            # Order the categories in a variables from that with the lowest
+            # house sale price, to that with the highest
+            ordered_labels = tmp.groupby([var])[target].mean().sort_values().index
+
+            # Create a dictionary of ordered categories to integer values
+            ordinal_label = {k: i for i, k in enumerate(ordered_labels, 0)}
+
+            print(var, ordinal_label)
+
+            # use the dictionary to replace the categorical strings by integers
+            train[var] = train[var].map(ordinal_label)
+            test[var] = test[var].map(ordinal_label)
+
+        for var in cat_others:
+            replace_categories(
+                self.x_train, self.x_test, self.y_train, var, "SalePrice"
+            )
+        # Checking
+        null = [
+            var for var in self.x_train.columns if self.x_train[var].isnull().sum() > 0
+        ]
+        print(null)
+        null = [
+            var for var in self.x_test.columns if self.x_test[var].isnull().sum() > 0
+        ]
         print(null)
 
 
 if __name__ == "__main__":
     model = HousePricePrediction()
     model.feature_engineering()
-
-
-user_input = input("Enter your dish name: ")
-print(user_input)
